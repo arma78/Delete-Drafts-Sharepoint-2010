5c995e0945f17ff77103d0df68e8cd576ef5b0e6
Armin Razic <37850817+arma78@users.noreply.github.com>
2018-09-14 18:47:51 +0200

    Add files via upload

---
+++ DeleteDrafts_EventReceiver/DeleteDrafts_EventReceiver/DeleteDrafts_EventReceiver.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Security;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+using Microsoft.Office.DocumentManagement.DocumentSets;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace DeleteDrafts_EventReceiver.DeleteDrafts_EventReceiver
+{
+    /// <summary>
+    /// List Item Events
+    /// </summary>
+    public class DeleteDrafts_EventReceiver : SPItemEventReceiver
+    {
+        /// <summary>
+        /// An item was added.
+        /// </summary>
+        public override void ItemAdded(SPItemEventProperties properties)
+        {
+            Thread.Sleep(60000);            
+            
+            base.EventFiringEnabled = false;
+            SPSecurity.RunWithElevatedPrivileges(delegate()
+              {
+                  using (SPSite site = new SPSite(properties.WebUrl))
+                  {
+                      using (SPWeb web = site.OpenWeb())
+                      {
+                          SPList oList = properties.Web.Lists["ReviewDocs"];
+                          string itemId = properties.ListItem["ID"].ToString();
+                          SPListItem sourceItem = oList.GetItemById(Convert.ToInt32(itemId));
+                                                
+                                                     
+                              SPView oView = oList.Views["Delete"]; 
+                              SPQuery query = new SPQuery(oView); 
+                              SPListItemCollection listItemsCollection = oList.GetItems(query);
+
+                              for (int i = listItemsCollection.Count - 1; i >= 0; i--)
+			                                    {                                                   
+                                                    SPListItem file = listItemsCollection[i];                              
+                                                               file.File.Delete();
+			                                    }                                                           
+                      }
+
+                  }
+              });
+           
+            base.EventFiringEnabled = true;
+        }
+    }
+}
+
+    
+
---
+++ DeleteDrafts_EventReceiver/Features/DeleteDrafts_EventReceiver_Feature/DeleteDrafts_EventReceiver_Feature.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="e9c7424b-78d1-49c9-82b5-87fca16cbbfb" description="Event Handler on &quot;Reviw files&quot; document library using itemAdded event to delete draft files&#xD;&#xA;Delete files from &quot;Review Files&quot;  document library filtered based on specific view." featureId="e9c7424b-78d1-49c9-82b5-87fca16cbbfb" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="DeleteDrafts_EventReceiver Feature" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="c950bc9e-7698-420a-b574-10b273ebbdab" />
+  </projectItems>
+</feature>
